43f10116d6fdc137b4da1aa4d9c96dd439b35946
Raymund Laude <laude320@gmail.com>
2024-10-26 17:48:44 +0800

    Add files via upload

---
+++ sinesugbowatch/src/main/java/com/g2appdev/sinesugbowatch/entity/Admin.java
@@ -0,0 +1,63 @@
+package com.g2appdev.sinesugbowatch.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int adminId;
+    private String username;
+    private String email;
+    private String password;
+
+    // Constructors
+    public Admin() {
+    	super();
+    }
+    	
+    public Admin(int adminId, String username, String email, String password) {
+    	super();
+    	this.adminId = adminId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
