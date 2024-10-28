b4851df4565338e8531048b5c9a708d9718de650
B4rryAli <christianbarryalico@gmail.com>
2024-10-28 22:23:32 +0800

    Update SearchRepository.java

--- sinesugbowatch/src/main/java/com/g2appdev/sinesugbowatch/repository/SearchRepository.java
+++ sinesugbowatch/src/main/java/com/g2appdev/sinesugbowatch/repository/SearchRepository.java
@@ -2,10 +2,10 @@ package com.g2appdev.sinesugbowatch.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.g2appdev.sinesugbowatch.entity.Search;
+import com.g2appdev.sinesugbowatch.entity.SearchEntity;
 
 @Repository
-public interface SearchRepository extends JpaRepository<Search, Integer> {
+public interface SearchRepository extends JpaRepository<SearchEntity, Integer> {
     // Method to find Search by search query
-    Search findBySearchquery(String searchquery);
+	SearchEntity findBySearchquery(String searchquery);
 }
