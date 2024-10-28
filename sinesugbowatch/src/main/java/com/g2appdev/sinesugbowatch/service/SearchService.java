e7c81ff70ef81ed2a1e2577e65ba666fd927d4d3
B4rryAli <christianbarryalico@gmail.com>
2024-10-28 22:24:42 +0800

    Update SearchService.java

--- sinesugbowatch/src/main/java/com/g2appdev/sinesugbowatch/service/SearchService.java
+++ sinesugbowatch/src/main/java/com/g2appdev/sinesugbowatch/service/SearchService.java
@@ -1,11 +1,62 @@
-package com.g2appdev.sinesugbowatch.repository;
+package com.g2appdev.sinesugbowatch.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.g2appdev.sinesugbowatch.entity.Search;
+import java.util.List;
+import java.util.NoSuchElementException;
 
-@Repository
-public interface SearchRepository extends JpaRepository<Search, Integer> {
-    // Method to find Search by search query
-    Search findBySearchquery(String searchquery);
+import javax.naming.NameNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.g2appdev.sinesugbowatch.entity.SearchEntity;
+import com.g2appdev.sinesugbowatch.repository.SearchRepository;
+
+@Service
+public class SearchService {
+
+    @Autowired
+    SearchRepository searchRepo;
+
+    // Create of CRUD
+    public SearchEntity postSearchRecord(SearchEntity search) {
+        return searchRepo.save(search);
+    }
+
+    // Read of CRUD
+    public List<SearchEntity> getAllSearches() {
+        return searchRepo.findAll();
+    }
+
+    // Update of CRUD
+    @SuppressWarnings("finally")
+    public SearchEntity putSearchDetails(int id, SearchEntity newSearchDetails) {
+        SearchEntity search = new SearchEntity();
+        try {
+            // Search for search record by ID
+            search = searchRepo.findById(id).orElseThrow(() -> 
+                new NameNotFoundException("Search " + id + " not found")
+            );
+
+            // Update the fields with new values
+            search.setSearchquery(newSearchDetails.getSearchquery());
+            search.setSearchdate(newSearchDetails.getSearchdate());
+
+        } catch (NoSuchElementException nex) {
+            throw new NameNotFoundException("Search " + id + " not found");
+        } finally {
+            return searchRepo.save(search);
+        }
+    }
+
+    // Delete of CRUD
+    public String deleteSearch(int id) {
+        String msg = "";
+        if (searchRepo.existsById(id)) {
+            searchRepo.deleteById(id);
+            msg = "Search record successfully deleted!";
+        } else {
+            msg = id + " Not Found!";
+        }
+        return msg;
+    }
 }
