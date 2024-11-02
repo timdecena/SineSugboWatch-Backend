4cdd9988e7059156c693845388b368b846430f9a
B4rryAli <christianbarryalico@gmail.com>
2024-11-02 20:40:13 +0800

    fixed search

--- sinesugbowatch/src/main/java/com/g2appdev/sinesugbowatch/controller/SearchController.java
+++ sinesugbowatch/src/main/java/com/g2appdev/sinesugbowatch/controller/SearchController.java
@@ -1,7 +1,9 @@
 package com.g2appdev.sinesugbowatch.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.g2appdev.sinesugbowatch.entity.SearchEntity;
 import com.g2appdev.sinesugbowatch.service.SearchService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(method = RequestMethod.GET, path = "/api/search")
 public class SearchController {
