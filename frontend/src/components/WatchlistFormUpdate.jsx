440f09ec623a9e3fd77528668da7c70d92f8650f
tyronebeldad123 <tyronebeldad123@gmail.com>
2024-12-02 23:09:08 +0800

    watchlistupdate.css
    watchllist update css design added

--- frontend/src/components/WatchlistFormUpdate.jsx
+++ frontend/src/components/WatchlistFormUpdate.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
+import '../assets/WatchlistFormUpdate.css';
 
 const WatchlistFormUpdate = () => {
   const { id } = useParams();
